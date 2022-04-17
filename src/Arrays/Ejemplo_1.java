837ebf4b36df6b07f76dd166f6c68a834aae3812
Eliseo-canaviri <53970655+Eliseo-canaviri@users.noreply.github.com>
2022-04-16 23:29:34 -0400

    Cargando el archivos

---
+++ src/Arrays/Ejemplo_1.java
@@ -0,0 +1,22 @@
+/*
+ Ejemplo 1
+  Crear un array que contenga los numeros de 1 al 10. Mostrar por la  pantalla
+ */
+package Arrays;
+
+/**
+ *
+ * @author Eliseo Canaviri
+ */
+public class Ejemplo_1 {
+
+    public static void main(String[] args) {
+        int num[] = new int[10];
+        //////System.out.println(" " + num[1]);
+        for (int i = 0; i < num.length; i++) {
+            num[i] = i + 1;
+            System.out.print("[ " + num[i] + " ]");
+        }
+        System.out.println("\n");
+    }
+}
---
+++ src/Arrays/Ejemplo_3.java
@@ -0,0 +1,24 @@
+/*
+  mostrar n productos con sus respectivos precios.
+ */
+package Arrays;
+
+/**
+ *
+ * @author Eliseo Canaviri
+ */
+public class Ejemplo_3 {
+
+    public static void main(String[] args) {
+
+        String utiles[] = {"pelota", "zapatilla", "mochila", "polo"};
+        double precios[] = {35.5, 89.60, 45.99, 25.70};
+
+        System.out.println("7777777" + precios.length);
+        System.out.println("Lista de Productos y sus precios");
+        for (int u = 0; u < utiles.length; u++) {
+            System.out.println(utiles[u] + "= " + precios[u]);
+        }
+    }
+
+}
